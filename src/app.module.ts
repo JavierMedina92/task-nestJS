535f9deb25fd8d50d45e64fafe8fcf84c6215ba5
JavierMedina92 <javiermedina0192@gmail.com>
2024-03-18 14:52:32 -0700

    se crea tabla de usuarios

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+  TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: '',
+    database: 'tasklist',
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize: true
+  }),
+  TasksModule,
+  UsersModule],
   controllers: [],
   providers: [],
+  
 })
 export class AppModule {}
